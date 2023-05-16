82721d0d68d1b8134158f766ab10a5ac709038b1
alexandervo <vokienphu@gmail.com>
2023-05-16 12:30:45 +0700

    sửa lại giao diện tab và trang chủ momo

--- App.js
+++ App.js
@@ -1,29 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {MOMO} from './components/MOMO';
 import {CHAT} from './components/CHAT';
 import {LSGD} from './components/LSGD';
 import {VCT} from './components/VCT';
-
+import Ionicons from '@expo/vector-icons/Ionicons';
+import * as React from 'react';
 
 
 const Tab = createBottomTabNavigator();
+
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <NavigationContainer style={styles.bottomTab}>
-        <Tab.Navigator >
-          <Tab.Screen name="MOMO" component={MOMO} options={{title:"MOMO"}}/>
-          <Tab.Screen name="LSGD" component={LSGD} options={{title:"LỊCH SỬ GD"}}/>
-          <Tab.Screen name="CHAT" component={CHAT} options={{title:"CHAT"}}/>
-          <Tab.Screen name="VCT" component={VCT} options={{title:"VÍ CỦA TÔI"}}/>
-          
-        </Tab.Navigator>
-      </NavigationContainer>
+      <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={MOMO}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
+            if (rn === "LSGD") {
+              iconName = focused ? 'time' : 'time';
+
+            } else if (rn === "CHAT") {
+              iconName = focused ? 'chatbox-ellipses' : 'chatbox-ellipses-outline';
+
+            } else if (rn === "VCT") {
+              iconName = focused ? 'person' : 'person';
+
+            } else if (rn === "MOMO") {
+              iconName = focused ? 'home' : 'home';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;                
+          },
+          "tabBarActiveTintColor": "#D82D8B",
+          "tabBarInactiveTintColor": "grey",
+          "tabBarLabelStyle": {
+          "paddingBottom": 10,
+          "fontSize": 10
+          },
+          "tabBarStyle": [
+            {
+              "display": "flex"
+            },
+            null
+          ],
+          
+        })}>
+          <Tab.Screen name="MOMO" component={MOMO} options={{tabBarLabel: "MOMO"}}/>
+          <Tab.Screen name="LSGD" component={LSGD} options={{tabBarLabel: "LỊCH SỬ GD"}}/>
+          <Tab.Screen name="CHAT" component={CHAT} options={{tabBarLabel: "CHAT"}}/>
+          <Tab.Screen name="VCT" component={VCT} options={{tabBarLabel: "VÍ CỦA TÔI"}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
     </View>
   );
 }
@@ -34,6 +69,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: Platform.OS === 'android' || Platform.OS ==='ios' ? 'row': 'column',
 
   },
   bottomTab : {
--- components/MOMO.js
+++ components/MOMO.js
@@ -1,30 +1,126 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import Constants from 'expo-constants';
+import Ionicons from '@expo/vector-icons/Ionicons';
 export const MOMO = ({navigation}) =>{
-  
+  const [isAmountVisible, setIsAmountVisible] = useState(false);
+  const amount = '100,000'; // Số tiền hiển thị
+  const toggleAmountVisibility = () => {
+    setIsAmountVisible(!isAmountVisible);
+  };
   return(
-    <View style={styles.paragraph}>
-      <Text>
-        Hahahahahahahaahahhahahahahahahahahaha
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.header_email}>
+          <Text style={styles.email}>
+            account_name@gmail.com
+          </Text>
+          
+        </View>
+        <TouchableOpacity>
+          <Image style={styles.bell} source={require('../src/image/bell.png')}/>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.b_account} source={require('../src/image/basic_account.png')}/>
+        </TouchableOpacity>
+        
+      </View>
+      <View style={styles.body}>
+        <View style={styles.features}>
+          <TouchableOpacity>
+            <Image source={require('../src/image/24_navigation_cash_in.png')} style={{tintColor: '#D82D8B', height: 100, width: 100, }}/>
+            <Text style={styles.font_feature}>{'Nạp tiền\nvào ví'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('../src/image/bank.png')} style={styles.features_icon}/>
+            <Text style={styles.font_feature}>{'Chuyển tiền\nngân hàng'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={require('../src/image/smartphone.png')} style={styles.features_icon}/>
+            <Text style={styles.font_feature}>{'Nạp tiền\nđiện thoại'}</Text>
+          </TouchableOpacity>
+
+        </View>
+        <View style={styles.amount_money}>
+
+          <TouchableOpacity onPress={toggleAmountVisibility}>
+            <Ionicons style={styles.eye} name={isAmountVisible ? 'eye' : 'eye-off'} size={40}/>
+          </TouchableOpacity>
+          <Text style={styles.amount}>{isAmountVisible ? amount +'đ' : '*********'}</Text>
+        </View>
+      </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    //justifyContent: 'center',
+    //paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#3c5b49',
     padding: 8,
   },
-  paragraph: {
+  header_email: {
+    width: '70%',
     padding: 8,
     borderRadius: 15,
-    backgroundColor: 'white',
+    backgroundColor: 'rgba(0,0,0, 0.6)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
   },
-  wrapper: {
-    paddingBottom: 10,
+  email: {
+    color: 'white',
+  },
+  header: {  
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bell: {
+    height: 34,
+    width: 34,
+    padding: 8, 
+    backgroundColor: 'rgba(0,0,0, 0.6)',
+    borderRadius: 13,
+    left: 20,
+    
+  },
+  b_account: {
+    height: 34,
+    width: 34,
+    padding: 8, 
+    backgroundColor: 'rgba(0,0,0, 0.6)',
+    borderRadius: 13,
+  },
+  amount_money: {
+    flexDirection: 'row',
+    top: 60,
+  },
+  amount: {
+    fontSize: 40,
+    left: 20,
+  },
+  eye:{
+    top: 5,
+    left: 10,
+  },
+  body: {
+    top: 20,
+    backgroundColor: '#e0e0e1',
+    borderRadius: 15,
+    paddingBottom: '20%',
+    flexDirection: 'column',
+  },
+  features: {
+    flexDirection: 'row',
+    top: 20,
+    left: 5,
+    justifyContent: 'space-around',
+  },
+  features_icon: {
+    height: 100,
+    width: 100,
+  },
+  font_feature: {
+    fontSize: 20,
   }
 });
